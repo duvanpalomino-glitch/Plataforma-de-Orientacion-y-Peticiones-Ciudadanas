d474ac978024d9c0608f5a937892f566b23160f0
duvanpalomino-glitch <juandiegojuliopalomino@DiegoRemix.local>
2026-07-06 20:22:42 -0500

    clase 52

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const path = require("path");
 const peticionesRoutes = require("./src/routes/peticiones.routes");
 const authRoutes = require("./src/routes/auth.routes");
+const dashboardRoutes = require("./src/routes/dashboard.routes");
+
 
 
 
@@ -20,6 +22,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/peticiones", peticionesRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+
 
 
 app.get("/", (req, res) => {
